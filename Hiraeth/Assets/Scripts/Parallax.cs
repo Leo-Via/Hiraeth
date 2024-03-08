95f9b009f894b4656d04adf0b2f7be9f39796afe
Sze0921HY <melloyeung0921@knights.ucf.edu>
2024-03-08 01:31:02 -0500

    Update Parallax.cs
    now will be repeated

--- Hiraeth/Assets/Scripts/Parallax.cs
+++ Hiraeth/Assets/Scripts/Parallax.cs
@@ -19,7 +19,12 @@ public class Parallax : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
         float dist = (cam.transform.position.x * parallaxEffect);
+
         transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+        if (temp > startpos + length) startpos += length;
+        else if (temp < startpos - length) startpos -= length;
     }
 }
