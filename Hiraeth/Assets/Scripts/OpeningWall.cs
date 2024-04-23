5218230cb700ab4677c48588c9d8d5caf71822ac
Connor34Bennett <co617187@ucf.edu>
2024-04-23 00:34:59 -0400

    Opening Walls after defeating all the enemies

---
+++ Hiraeth/Assets/Scripts/OpeningWall.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class OpeningWall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject play;
+    GameObject[] enemies;
+
+    GameObject w;
+    TilemapRenderer ren;
+    TilemapCollider2D c;
+    CompositeCollider2D coll;
+
+
+
+    void Start()
+    {
+        play = GameObject.FindGameObjectWithTag("Player");
+        w = GameObject.FindGameObjectWithTag("OpeningWall");
+        ren = w.GetComponent<TilemapRenderer>();
+        c = w.GetComponent<TilemapCollider2D>();
+        coll = w.GetComponent<CompositeCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        if(enemies.Length == 0){
+            ren.enabled = false;
+            c.enabled = false;
+            coll.enabled = false;
+        }
+    }
+}
