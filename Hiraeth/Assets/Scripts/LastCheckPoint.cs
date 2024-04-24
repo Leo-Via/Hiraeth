37bd97906853a350fbe7627cb80ca88aa7eb4e22
RyWorrell <159931389+RyWorrell@users.noreply.github.com>
2024-04-24 05:06:50 -0400

    Tweaks

--- Hiraeth/Assets/Scripts/LastCheckPoint.cs
+++ Hiraeth/Assets/Scripts/LastCheckPoint.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class LastCheckPoint : MonoBehaviour
+public class LastCheckPoint : MonoBehaviour, IDataPersistence
 {
     private static LastCheckPoint instance;
     public Vector2 lastCheckPointPos;
@@ -18,6 +19,17 @@ public class LastCheckPoint : MonoBehaviour
         }
         
     }
+
+    public void LoadData(GameData data)
+	{
+		this.lastCheckPointPos = data.playerPosition;
+	}
+
+	public void SaveData(GameData data)
+	{
+		data.playerPosition = this.lastCheckPointPos;
+        data.LevelNum = SceneManager.GetActiveScene().name;
+	}
 
     // Update is called once per frame
     void Update()
